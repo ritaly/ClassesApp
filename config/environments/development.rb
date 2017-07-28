0a4390d3f5a1b461898403dc3d28eb2c5d1ab1e4
ritaly <malgorzata.lyczywek@gmail.com>
2017-07-28 10:43:54 +0200

    feature send mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,15 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+     config.action_mailer.smtp_settings = {
+        :address => "smtp.gmail.com",
+        :port => 587,
+        :domain => "wzoryikolory.pl",
+        :authentication => :plain,
+        :user_name => "usertestrails@gmail.com",
+        :password => "TrudneHaslo1"
+     }
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
